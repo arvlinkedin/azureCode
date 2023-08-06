3433ae5d76a77191ef7dd39ad71eb6bbd89555ff
Arvind Kumar <33369515+codeCompile@users.noreply.github.com>
2023-08-06 19:56:51 +0530

    'test'

---
+++ sprinbootappkubernetes/src/main/java/com/appkubernetes/sprinbootappkubernetes/SprinbootappkubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.appkubernetes.sprinbootappkubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbootappkubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinbootappkubernetesApplication.class, args);
+	}
+
+}
